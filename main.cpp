726b779cf804a4d95e792cc58454925c165021f9
wangli-git-code <63201497+wangli-git-code@users.noreply.github.com>
2020-11-04 11:05:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,124 +2,108 @@
 #include<cstring>
 #include<fstream>
 using namespace std;
-char token[55];
-char in[55];
-char s[55];
+char token[1030];
+char ans[1030];
+int op[6][6] = {{2,1,1,1,2,2},{2,2,1,1,2,2},{2,2,-1,-1,2,2},{1,1,1,1,0,-1},{2,2,-1,-1,2,2},{1,1,1,1,-1,-1}};
+//+ * i ( ) #
 FILE *fin,*fout;
-int cnt = 0,token_num = 0;
-int row = 1;
-int flag = 0;
-char ch;
-char keyWord[7][55] = {"BEGIN","END","FOR","IF","THEN","ELSE"};
-char keyWord1[7][55] = {"Begin","End","For","If","Then","Else"};
-char a1[55]="LParenthesis";
-char a2[55]="RParenthesis";
-char a3[55]="Plus";
-char a4[55]="Star";
-char a5[55]="Comma";
-char a6[55]="Assign";
-char a7[55]="Colon";
-char a8[55]="Unknown";
-char zero[2]="0";
+int cnt=0;
+int p=-1;
+int q;
+int s=0;
+char a;
+char b;
+char c;
+char d,ch;
+int i,j,k;
+char e;
+int hash[200];
 void init_token(){
 	int i;
-	for(i = 0;i < 55;i++){
-		token[i] = 0;
+	for(i = 0;i < 1024;i++){token[i] = 0;}
+	cnt=0;
+	while((ch=getc(fin))!='\n'){
+		token[cnt++]=ch;
 	}
+	token[cnt++]='#';
 }
 
-char* judge_token(){
+	
+	
+int main(int argc, char **argv){
+	fin = fopen(argv[1],"r");
 	init_token();
-	if(flag == 0){
-		ch = getc(fin);
-	}
-	flag = 1;
-	while(ch == ' ' || ch == '\t' || ch == '\n'){
-		if(ch == '\n'){
-			row++;
+	hash['+']=0;
+	hash['*']=1;
+	hash['i']=2;
+	hash['(']=3;
+	hash[')']=4;
+	hash['#']=5;
+	ans[++p]='#';
+	
+	while(true){
+		q=p;
+		while(ans[q]=='n'){
+			q--;
 		}
-		ch=getc(fin);
-	}
-	token_num = 0;
-	if((ch>='a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z')){
-		while((ch>='a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z') || (ch >= '0' && ch <= '9')){
-			token[token_num++] = ch;
-			ch = getc(fin);
+		a=ans[q];
+		b=token[s];
+		if(op[hash[a]][hash[b]]==1||op[hash[a]][hash[b]]==0){
+			ans[++p]=b;
+			s++;
+			printf("I%c\n",b);
 		}
-		token[token_num++] = '\0';
-		for(int i = 0;i <7;i++){
-			if(strcmp(token,keyWord[i]) == 0){
-				return keyWord1[i];
-			}
-		}
-		for(int j=0;j<55;j++) s[j]=0;
-				strcat(s,"Ident(");
-                strcat(s,token);
-                strcat(s,")");
-		return s;
-	}
-	else if(ch >= '0' && ch <= '9'){
-		while(ch >= '0'&& ch <= '9'){
-			token[token_num++] = ch;
-			ch = getc(fin);
-		}
-		for(int j=0;j<55;j++) s[j]=0;
-				strcat(s,"Int(");
-				int k=0;
-				int t=0;
-				for(int m=0;m<strlen(token);m++){
-					if(token[m]=='0'){
-						k=m;
+		else if(op[hash[a]][hash[b]]==2){
+			while(true){
+				int times=0;
+				for(i=p;i>=0;i--){
+					if(ans[i]!='n'&&times==0) {
+						c=ans[i];
+						times++;
 					}
-					else {
-						k=m;
-						t=1;
+					else if(ans[i]!='n'&&times==1){
+						d=ans[i];
 						break;
 					}
 				}
-				if(t==0) strcat(s,zero);
-                else strcat(s,token+k);
-                strcat(s,")");
-		return s;
-	}
-	else{
-		token[token_num++] = ch;
-		switch(ch){
-			case '(': ch = getc(fin); return a1;
-			case ')': ch = getc(fin); return a2;
-			case '+': ch = getc(fin); return a3;
-			case '*': ch = getc(fin); return a4;
-			case ',': ch = getc(fin); return a5;
-			case ':':{
-				ch = getc(fin);
-				if(ch == '='){
-					token[token_num++] = ch;
-					ch = getc(fin);
-					return a6;
+				k=i;
+				if(op[hash[d]][hash[c]]==0){
+					for(j=i-1;j>=0;j--){
+						if(ans[j]!='n') {
+							e=ans[j];
+						}
+					}
+					c=d;
+					d=e;
+					k=j;
 				}
-				else{
-					return a7;
+				else if(op[hash[d]][hash[c]]==1){
+					break;
+				}
+				else if(op[hash[d]][hash[c]]==-1){
+					printf("E\n");
+					return 0;
 				}
 			}
-			case EOF: return NULL;
-			default: ch = getc(fin); return a8;
+			
+			
+			p=k+1;
+			ans[p]='n';
+			printf("R\n");
+			if(p==1&&ans[0]=='#'&&ans[1]=='n'&&token[s]=='#'){
+				return 0;
+			}
+//			else if(token[s]=='#'&&p!=1){
+//				
+//			}
 		}
+		else if(op[hash[a]][hash[b]]==-1){
+			printf("E\n");
+					return 0;
+		}
+			
 	}
-}
 
-int main(int argc, char **argv){
-	fin = fopen(argv[1],"r");
-	char *temp=NULL;
-	while(1){
-		temp = judge_token();
-		if(temp==NULL){
-			return 0;
-		}
-		else if(strcmp(temp,a8)==0){
-			cout<<a8<<endl;
-			return 0;
-		}
-		else cout<<temp<<endl;
-	}
+	
 	return 0;
 }
